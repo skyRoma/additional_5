818e9fe4660d34c95b0e674f8c5b5c2e8c2f1813
roman <roma.sluka.97@mail.ru>
2018-02-23 14:44:24 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var stack = [];
+    var lastInStack;
+    var mas = str.split('');
+    var solution = true;
+    if (mas.length % 2 != 0) return false;
+    for (var k = 0; k < mas.length; k++) {
+        for (var i = 0; i < bracketsConfig.length; i++) {
+            if (mas[k] == bracketsConfig[i][1] && stack[stack.length - 1] == bracketsConfig[i][0]) {
+                stack.pop();
+            } else if (mas[k] == bracketsConfig[i][0]) {
+                stack.push(mas[k]);
+            }
+        }
+    }
+    if (stack.length != 0) solution = false;
+    return solution;
 }
